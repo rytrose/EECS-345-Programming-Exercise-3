ce749fbde431f51b2cf3a5f2cebca0c8f7e2c9d2
Ryan Rose <rytrose@gmail.com>
2017-04-18 01:54:31 -0400

    myreverse works, flatten is nearly working...

--- types.hs
+++ types.hs
@@ -1,5 +1,38 @@
-data NestedList a = Elem a | List [NestedList a]
+data NestedList a = Element a | SubList [NestedList a] deriving (Show)
 
-flatten :: NestedList a -> [a]
-flatten (Elem x) = [x]
-flatten (List x) = concatMap flatten x
+flatten :: [NestedList a] -> [NestedList a]
+flatten l =
+  if (null l)
+    then
+      []
+  else if (isElement (head l))
+    then
+      (head l) : (flatten (tail l))
+  else
+      (head (flatten (getList (head l)))) : (flatten (tail l))
+
+
+myreverse :: [NestedList a] -> [NestedList a]
+myreverse l =
+  if (null l)
+    then
+      []
+    else
+      if (isSubList (head l))
+        then
+          (SubList (myreverse (getList (last l)))) : (myreverse (init l))
+        else
+          (last l) : (myreverse (init l))
+
+getSlHead (SubList sl) = (head sl)
+getSlTail (SubList sl) = (tail sl)
+getSlInit (SubList sl) = (init sl)
+getSlLast (SubList sl) = (last sl)
+
+isElement (Element _) = True
+isElement _ = False
+isSubList (SubList _) = True
+isSubList _ = False
+
+getList :: NestedList a -> [NestedList a]
+getList sl = (getSlHead sl) : (getSlTail sl)
