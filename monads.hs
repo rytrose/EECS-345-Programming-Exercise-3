f4b0d06faece19bd90d3115ef22fed74e5f275b0
Ryan Rose <rytrose@gmail.com>
2017-04-19 16:31:13 -0400

    part 3 is done entirely

--- monads.hs
+++ monads.hs
@@ -21,3 +21,12 @@ checklist x check =
     Just ([])
   else
     appendIf (head x) (checklist (tail x) check) check
+
+checkappend :: Maybe [a] -> Maybe [a] -> (a -> Bool) -> Maybe [a]
+checkappend Nothing ml2 check = Nothing
+checkappend ml1 Nothing check = Nothing
+checkappend ml1 ml2 check =
+  if (isNothing (checklist (fromJust ml1) check)) then
+    Nothing
+  else
+    Just ((fromJust (checklist (fromJust ml1) check)) ++ (fromJust ml2))
